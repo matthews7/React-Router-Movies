8bc5c0e4b3e1a179786775a100f88ddafbac776d
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2019-12-16 14:47:14 -0700

    Part one routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-
+import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -12,7 +13,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/">
+        <MovieList/>
+      </Route>
+
+      <Route exact path="/movies/:id">
+        <Movie/>
+      </Route>
+      {/* <div>Replace this Div with your Routes</div> */}
     </div>
   );
 };
