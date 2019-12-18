d864524dc75168ca11804befcb6d82a97a67995c
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2019-12-17 18:40:43 -0700

    mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -39,23 +39,6 @@ function MovieDetails(props) {
   const movie = props.movie;
   console.log(movie);
   return (
-    // <div className="movie-card">
-     
-    //   <h2>{title}</h2>
-    //   <div className="movie-director">
-    //     Director: <em>{director}</em>
-    //   </div>
-    //   <div className="movie-metascore">
-    //     Metascore: <strong>{metascore}</strong>
-    //   </div>
-    //   <h3>Actors</h3>
-      
-    //   {stars.map(star => (
-    //     <div key={star} className="movie-star">
-    //       {star}
-    //     </div>
-    //   ))}
-    // </div>
     
   <Link to={`/movies/${movie.id}`}>
       <MovieCard movie={movie}/>
