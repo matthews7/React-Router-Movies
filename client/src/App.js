45872ddcbd317e6933046fa4aedffae337f30bda
Ammon Matthews <29493963+matthews7@users.noreply.github.com>
2019-12-17 18:35:37 -0700

    cards showing by id

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Route} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
@@ -12,14 +12,21 @@ const App = () => {
 
   return (
     <div>
+      
+      {/* <SavedList list={savedList} />
+      <Switch>
+        <Route path="/movies/:id" >
+          <Movie/>
+        </Route>
+        <Route path="/" >
+          <MovieList/>
+        </Route>
+      </Switch> */}
+      
       <SavedList list={savedList} />
-      <Route exact path="/">
-        <MovieList/>
-      </Route>
-
-      <Route exact path="/movies/:movieID">
-        <Movie/>
-      </Route>
+      <Route exact path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ Movie } />
+      
       {/* <div>Replace this Div with your Routes</div> */}
     </div>
   );
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from "react-router-dom"
 
-const Movie = (props) => {
+
+const Movie = props => {
   const [movie, setMovie] = useState();
  
+
+ 
+  
+
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,13 +24,17 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
   //   addToSavedList(movie)
   // }
+
+  
+  
+  // console.log(movie);
 
   if (!movie) {
     return <div>Loading movie information...</div>;
